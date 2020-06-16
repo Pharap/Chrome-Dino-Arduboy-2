5b58f27debe0df91c08c6825f31b07f9b544c028
Pharap <2933055+Pharap@users.noreply.github.com>
2020-06-16 10:27:30 +0100

    Add updateGameplay and drawGameplay

--- Chome_Dino_NEW/Chome_Dino_NEW.ino
+++ Chome_Dino_NEW/Chome_Dino_NEW.ino
@@ -80,6 +80,8 @@ void loop()
       break;
 
     case GameState::Gameplay:
+      updateGameplay();
+      drawGameplay();
       break;
 
     case GameState::GameOver:
@@ -125,6 +127,127 @@ void drawGameOver()
   arduboy.setCursor(40, 30);
   arduboy.print("Score:");
   arduboy.print(d);
+}
+
+void updateGameplay()
+{
+  if (d_tumble_t == 1)
+  {
+    gameState = GameState::GameOver;
+  }
+
+  ++frame;
+  if (frame>16000) frame = 0;
+
+  // increase distance whilst running
+  if (d_run && (++delta > 8)) {
+    delta = 0; ++d; 
+  }
+
+  // obstacles
+  if (d_run) {
+    ox -= (frame%2)*(d/100) + 2;
+    if (ox < -15) ox += 140 + random(80,160);
+  }
+
+  // jump!
+  if (!d_jump_t && arduboy.pressed(A_BUTTON)) {
+    d_jump_t = 1;
+    d_jump=5;
+
+    sound.tone(440, 40);
+
+  } else if (d_jump_t) {
+    if (d_jump_t == 3) sound.tone(880, 80);
+
+    ++d_jump_t;
+
+    if (d_jump_t<6) {
+      d_jump +=6;
+    } else if (d_jump_t<9) {
+      d_jump +=2;
+    } else if (d_jump_t<13) {
+      d_jump +=1;
+    } else if (d_jump_t == 16 || d_jump_t == 18) {
+      d_jump +=1;
+    } else if (d_jump_t == 20 || d_jump_t == 22) {
+      d_jump -=1;
+    } else if (d_jump_t>38) {
+      d_jump = 0;
+      d_jump_t = 0;
+    } else if (d_jump_t>32) {
+      d_jump -=6;
+    } else if (d_jump_t>29) {
+      d_jump -=2;
+    } else if (d_jump_t>25) {
+      d_jump -=1;
+    }
+  }
+
+  // hit detect
+  if (!d_tumble_t && ox > -15 && ox <12 && d_jump_t < 14) {
+    d_tumble_t = 1;    
+    sound.tone(25, 100);
+  }
+
+  if (d_tumble_t != 0) {
+    if (d_tumble_t == 1) {
+    } else if (d_tumble_t == 6) {
+      sound.tone(174, 200);
+    }
+
+    if (d_jump > -4) {
+      d_jump -= 1;
+      ox -= 1;
+    } else {
+      d_run = false;
+    }
+  }
+}
+
+void drawGameplay()
+{
+  // hud
+  arduboy.setCursor(50, 6);
+  arduboy.print(d);
+  // parallax clouds
+  arduboy.drawBitmap(128 -(d%128),cloud_1_y,cloud_1,20,10,WHITE);
+
+  if (d%128 == 0) {
+    cloud_1_y = random(0,10);
+  }
+
+  // terrain
+  if (d_jump > 4) {
+    arduboy.drawLine(0,60,127,60,WHITE);
+  } else {
+    arduboy.drawLine(0,60,3,60,WHITE);
+    arduboy.drawLine(12,60,127,60,WHITE); 
+  }
+
+  // obstacles
+  arduboy.drawBitmap(ox,40,cactus_1,12,24,WHITE);
+
+  // dino
+  int dy = 40-d_jump;
+  // tumbles!
+  if (d_tumble_t != 0) {
+    arduboy.drawBitmap(0,dy+3,dino_tumble,30,18,WHITE);
+  // runs!
+  } else {
+    arduboy.drawBitmap(0,dy,dino_top,20,18,WHITE);
+  
+    // Run, Dino, Run!
+    if (d_run && !d_jump) {
+      if ((frame%8)/4) {
+        arduboy.drawBitmap(0,dy+16,dino_leg_1,20,8,WHITE);
+      } else {
+        arduboy.drawBitmap(0,dy+16,dino_leg_2,20,8,WHITE);
+      }
+    } else {
+      arduboy.drawBitmap(0,dy+16,dino_leg_0,20,8,WHITE);
+    }
+  }
 }
 
 void oldLoop() {
